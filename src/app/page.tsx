aff8ef23fa24891fa20e7b0a8e237178cf94fc7b
yinjiaw2 <yinjiaw2@gmail.com>
2026-05-01 14:39:00 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <GroupIntroSection />
       <EcosystemSection />
+      <GroupIntroSection />
       <ProcessSection />
       <ServiceSection />
     </>
